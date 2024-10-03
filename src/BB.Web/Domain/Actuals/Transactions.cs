0cbebfe29167261ced9289e35edf778cafb7e57c
Will Hausman <will.jhausman@outlook.com>
2024-10-02 21:46:56 -0500

    set

--- src/BB.Web/Domain/Actuals/Transactions.cs
+++ src/BB.Web/Domain/Actuals/Transactions.cs
@@ -1,7 +1,5 @@
 namespace BB.Web.Domain.Actuals;
 
-// public record Transaction(Guid Id, DateOnly Date, Money Amount, Category Category, string? Note = null);
-
 public record TransactionPosted(DateOnly Date, Money Amount, Category Category, string Note);
 
 public class Transaction(Guid id, DateOnly date, Money amount, Category category, string? note)
@@ -15,5 +13,5 @@ public class Transaction(Guid id, DateOnly date, Money amount, Category category
     public DateOnly Date { get; private set; } = date;
     public Money Amount { get; private set; } = amount;
     public Category Category { get; private set; } = category;
-    public string? Note { get; set; }
+    public string? Note { get; private set; } = note;
 }
